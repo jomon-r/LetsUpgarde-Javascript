eb0154046fd7cb659819c2081dc1d775dcb8fc28
Jomon Raju <78149482+jomon-r@users.noreply.github.com>
2021-07-07 18:42:59 +0530

    Update Assignment_Day2_Pattern.js

--- Assignment_Day2_Pattern.js
+++ Assignment_Day2_Pattern.js
@@ -1,7 +1,7 @@
 // LetsUpgrade Javascript Assignment day 2 to print pattern 
 let pattern=""
 for(let i=0;i<5;i=i+1){
-  pattern += " *"
+  pattern += "* "
 }
 console.log(pattern)
 // output- * * * * *
