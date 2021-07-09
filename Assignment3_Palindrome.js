61e186bb008ccd0ea0d294ca0c065a1affe091cd
Jomon Raju <78149482+jomon-r@users.noreply.github.com>
2021-07-09 11:51:20 +0530

    Create Assignment3_Palindrome.js

---
+++ Assignment3_Palindrome.js
@@ -0,0 +1,20 @@
+s = "Madam Arora teaches malayalam"
+s = s.toLowerCase() + " "
+s1 = "", s2 = ""
+for (t of s) {
+    if (t != " ") {
+        s1 = s1 + t  //to make the same word
+        s2 = t + s2  //to reverse a word 
+    }
+    else {
+        if (s1 == s2) {  //check whether the word is a palindrome
+          console.log(s1)
+        }
+        s1 = ""
+        s2 = ""
+    }
+}
+
+//output - madam
+//         arora
+//         malayalam
